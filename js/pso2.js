50798727996480dec3ad542f06f93471d50e3516
kamitsuki_genya <projectmoon.2007@gmail.com>
2015-07-21 00:29:11 +0900

    base commit

--- js/pso2.js
+++ js/pso2.js
@@ -1,23 +1,19 @@
 $(function() {
-  var jsonDataUrl = "./data/arkuma.json";
-  var arkumaWord;
-  setWord = function($target){
-    var l = arkumaWord.length;
-    var r = Math.floor(Math.random()*l)
-    $($target).text(arkumaWord[r].text);
-  };
+  // var jsonDataUrl = "./data/arkuma.json";
+  // var arkumaWord;
+  // var successFunc = function(json) {
+  //   arkumaWord = json.word;
+  // };
+  // $.getJSON(jsonDataUrl,function(json) {
+  //   successFunc(json);
+  //   return;
+  // });
+  // var setWord = function($target){
+  //   var l = arkumaWord.length;
+  //   var r = Math.floor(Math.random()*l)
+  //   $($target).text(arkumaWord[r].text);
+  // };
 
-  var successFunc = function(json) {
-    console.info(json);
-    console.info(json.word);
-    console.info(json.word.length);
-    console.info(json.word[0]);
-    arkumaWord = json.word;
-  };
-  $.getJSON(jsonDataUrl,function(json) {
-    successFunc(json);
-    return;
-  });
   $("html").contextMenu('rmenu',{
     menuStyle: {
       border: '0',
@@ -31,12 +27,12 @@ $(function() {
     shadow:false
   });
   $("div.arkuma").hover(
-  function () {
-    setWord("div.Gchat");
-    $("div.Gchat").animate( {opacity: 'toggle' }, 100 );
-  },
-  function () {
-    $("div.Gchat").animate( {opacity: 'toggle' }, 100 );
-  }
-);
- });
+    function () {
+      // setWord("div.Gchat");
+      $("div.Gchat").animate( {opacity: 'toggle' }, 100 );
+    },
+    function () {
+      $("div.Gchat").animate( {opacity: 'toggle' }, 100 );
+    }
+  );
+});
