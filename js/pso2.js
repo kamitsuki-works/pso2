acc5424f7b83a872b8298d18647d6d37739302b2
kamitsuki_genya <projectmoon.2007@gmail.com>
2015-11-02 02:01:53 +0900

    プロフィール追加

--- js/pso2.js
+++ js/pso2.js
@@ -35,4 +35,58 @@ $(function() {
       $("div.Gchat").animate( {opacity: 'toggle' }, 100 );
     }
   );
+
+  $("ul.characters li.c01").on('click',function(){
+    $("img.name02").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.name03").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.name04").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.gengetsu").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    $("img.artemis").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    $("img.mugetsu").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    setTimeout(function(){
+      $("img.name01").css("transition","0.4s").css("opacity","1").css("left","0");
+      $("img.moonlight").css("transition","0.4s").css("opacity","1").css("right","-10px");
+    },300)
+  })
+
+  $("ul.characters li.c02").on('click',function(){
+    $("img.name01").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.name03").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.name04").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.moonlight").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    $("img.artemis").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    $("img.mugetsu").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    setTimeout(function(){
+      $("img.name02").css("transition","0.4s").css("opacity","1").css("left","0");
+      $("img.gengetsu").css("transition","0.4s").css("opacity","1").css("right","40px");
+    },300)
+  })
+
+  $("ul.characters li.c03").on('click',function(){
+    $("img.name01").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.name02").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.name04").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.moonlight").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    $("img.gengetsu").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    $("img.mugetsu").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    setTimeout(function(){
+      $("img.name03").css("transition","0.4s").css("opacity","1").css("left","0");
+      $("img.artemis").css("transition","0.4s").css("opacity","1").css("right","40px");
+    },300)
+  })
+
+  $("ul.characters li.c04").on('click',function(){
+    $("img.name01").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.name02").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.name03").css("transition","0.4s").css("opacity","0").css("left","-300px");
+    $("img.moonlight").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    $("img.gengetsu").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    $("img.artemis").css("transition","0.4s").css("opacity","0").css("right","-300px");
+    setTimeout(function(){
+      $("img.name04").css("transition","0.4s").css("opacity","1").css("left","0");
+      $("img.mugetsu").css("transition","0.4s").css("opacity","1").css("right","20px");
+    },300)
+  })
+
+
 });
