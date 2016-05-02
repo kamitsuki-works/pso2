208a29d61168e1e48d6fbad4d3cfe39f7459f2ff
kamitsuki_genya <projectmoon.2007@gmail.com>
2016-05-02 18:03:33 +0900

    candyBox 0.02

--- js/candy.js
+++ js/candy.js
@@ -6,34 +6,16 @@ $(function() {
   // ペットの情報を取得する。
   $.getJSON(jsonPetDataUrl,function(json) {
     summonPets = json.pets;
+    $("div.hoge").text(summonPets[0].name);
     return;
   });
 
-  $("div.hoge").text(summonPets[0].name);
 
 
 
 
-
-
-  var setWord = function($target){
-    var l = arkumaWord.length;
-    var r = Math.floor(Math.random()*l)
-    $($target).text(arkumaWord[r].text);
-  };
-
-
-  $("div.arkuma").hover(
-    function () {
-      setWord("div.Gchat");
-      $("div.Gchat").animate( {opacity: 'toggle' }, 100 );
-    },
-    function () {
-      $("div.Gchat").animate( {opacity: 'toggle' }, 100 );
-    }
-  );
 
 //===== 初回実行時 =====
-  setCandyBox();
+//  setCandyBox();
 
 });
